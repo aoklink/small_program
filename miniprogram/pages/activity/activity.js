00e91f7c7c3fbe04bf8f388d242e3db239d71448
yunhan <2670893251@qq.com>
2019-03-08 17:59:27 +0800

    v3

--- miniprogram/pages/activity/activity.js
+++ miniprogram/pages/activity/activity.js
@@ -7,11 +7,15 @@ Page({
    * 页面的初始数据
    */
   data: {
+    current: 2,
+    imgboxa: ['http://ll.linkfeeling.cn/img/ok.png', 'http://ll.linkfeeling.cn/img/ok.png', 'http://ll.linkfeeling.cn/img/ok.png'],
+    fontFamily: 'FT',
     nono: true,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     ppy: '',
     oka: true,
     smi: '-',
+    bgbg: false,
     hcc: '',
     skc: '-',
     mrun: '-',
@@ -24,6 +28,7 @@ Page({
     llxl: '-',
     pl: '',
     olo: '',
+    bg: true,
     spt: false,
     ww: {
       msg: "ok",
@@ -53,7 +58,22 @@ Page({
     weight: '',
     head_icon: '',
     height: '',
-    fontFamily: 'FT'
+    trt: 0,
+    fontFamily: 'FT',
+    yb: ['免费停车', '国际教练', '可同时容纳100人', '免费停车', '国际教练'],
+    act: [{
+      url: 'http://ll.linkfeeling.cn/img/ok.png',
+      price: '19.9',
+      tit: '新手体验课1节，价值299元'
+    }, {
+      url: 'http://ll.linkfeeling.cn/img/ok.png',
+      price: '19.9',
+      tit: '节后消脂减肥体验课，价值299元节后消脂减肥体验课，价值299元'
+    }, {
+      url: 'http://ll.linkfeeling.cn/img/ok.png',
+      price: '19.9',
+      tit: '新手体验课1节，价值299元'
+    }]
   },
   mycell: function () {
     wx.navigateTo({
@@ -71,20 +91,32 @@ Page({
       complete: function (res) { },
     })
   },
-  goselect: function () {
-    wx.navigateTo({
-      url: '../select/select',
-      success: function(res) {},
-      fail: function(res) {},
-      complete: function(res) {},
+  call: function () {
+    wx.makePhoneCall({
+      phoneNumber: '13654917649' //仅为示例，并非真实的电话号码
     })
   },
-  gohouse: function () {
+  gomap: function () {
     wx.navigateTo({
-      url: '../myhouse/myhouse',
-      success: function(res) {},
-      fail: function(res) {},
-      complete: function(res) {},
+      url: '../map/map',
+      success: function (res) { },
+      fail: function (res) { },
+      complete: function (res) { },
+    })
+  },
+  fullpage: function (event) {
+    var that = this
+    var ink = event.currentTarget.dataset['index'];
+    this.setData({
+      bg: false,
+      bgbg: true,
+      current: ink
+    })
+  },
+  fullno: function () {
+    this.setData({
+      bgbg: false,
+      bg: true
     })
   },
   /**
@@ -247,140 +279,154 @@ Page({
         getApp().globalData.userInfo = res.data;
       }
     })
-  }, 
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var that = this
-    wx.login({
-      //获取code
-      success: function (res) {
-        console.log(res)
-        that.setData({
-          hcc: res.code
-        })
+    // var that = this
+    // wx.login({
+    //   //获取code
+    //   success: function (res) {
+    //     console.log(res)
+    //     that.setData({
+    //       hcc: res.code
+    //     })
+    //   }
+    // }),
+    // wx.loadFontFace({
+    //   family: this.data.fontFamily,
+    //   source: 'url("https://www.linkfeeling.cn/platform/font/DIN 1451 Std Engschrift.TTF")',
+    //   success(res) {
+    //     console.log(res.status)
+    //   },
+    //   fail: function (res) {
+    //     console.log(res.status)
+    //   },
+    //   complete: function (res) {
+    //     console.log(res.status)
+    //   }
+    // }),
+    // wx.getNetworkType({
+    //   success: function (res) {
+    //     console.log(res.networkType);
+    //     us.nw = res.networkType
+    //   }
+    // })
+    // // 查看是否授权
+    // wx.getSetting({
+    //   success: function (res) {
+    //     if (res.authSetting['scope.userInfo']) {
+    //       wx.getUserInfo({
+    //         success: function (res) {
+    //           console.log("授权成功")
+    //           us.nickName = res.userInfo.nickName
+    //           that.setData({
+    //             pl: res.userInfo.nickName,
+    //             olo: res.userInfo.avatarUrl
+    //           })
+    //           us.avatarUrl = res.userInfo.avatarUrl
+    //           us.usname = res.userInfo.nickName
+    //           wx.login({
+    //             success: function (res) {
+    //               console.log(res.code)
+    //               wx.request({
+    //                 url: app.globalData.lp + 'user/login', // 仅为示例，并非真实的接口地址
+    //                 method: 'POST',
+    //                 data: {
+    //                   wx_code: that.data.hcc,
+    //                   user_type: us.ut,
+    //                   request_time: us.rt,
+    //                   platform: us.pt,
+    //                   tk: mmd.hexMD5(us.pi + ":" + us.ut + ":" + us.rt),
+    //                   login_type: "wx",
+    //                   network: us.nw,
+    //                   product_id: us.pi,
+    //                   app_version: us.av
+    //                 },
+    //                 header: {
+    //                   'content-type': 'application/json' // 默认值
+    //                 },
+    //                 success(res) {
+    //                   console.log(res.data)
+
+    //                   if (res.data.code == 103) {
+    //                     wx.navigateTo({
+    //                       url: '../cellnumber/cellnumber',
+    //                       // url: '../information/information',
+    //                       success: function (res) { },
+    //                       fail: function (res) { },
+    //                       complete: function (res) { },
+    //                     })
+    //                   }
+    //                   else if (res.data.code == 200) {
+    //                     console.log(res.data.data)
+    //                     us.uid = res.data.data.uid
+    //                     that.setData({
+    //                       nono: true,
+    //                     })
+    //                     wx.request({
+    //                       url: 'https://ll.linkfeeling.cn/api/user/account_info',
+    //                       method: 'POST',
+    //                       data: {
+    //                         uid: us.uid,
+    //                         user_type: us.ut,
+    //                         request_time: us.rt,
+    //                         platform: us.pt,
+    //                         tk: mmd.hexMD5(us.pi + ":" + us.ut + ":" + us.rt),
+    //                         login_type: "wx",
+    //                         network: us.nw,
+    //                         product_id: us.pi,
+    //                         app_version: us.av
+    //                       },
+    //                       header: {
+    //                         'content-type': 'application/json' // 默认值
+    //                       },
+    //                       success(res) {
+    //                         console.log(res.data)
+    //                         if (res.data.code == 200) {
+    //                           that.setData({
+    //                             head_icon: res.data.data.head_icon,
+    //                             head_icon: 'http://ll.linkfeeling.cn/img/ok.png',
+    //                             name: res.data.data.name,
+    //                             // sex: res.data.data.gender,
+    //                             // age: res.data.data.age,
+    //                             // goal: res.data.data.goal,
+    //                             // weight: res.data.data.weight,
+    //                             // height: res.data.data.stature
+    //                           })
+    //                         }
+    //                       }
+    //                     })
+    //                   }
+    //                 }
+    //               })
+    //             }
+    //           })
+    //         }
+    //       });
+    //     }
+    //     else {
+    //       that.setData({
+    //         nono: false
+    //       })
+    //     }
+    //   }
+    // })
+    //   ,
+    wx.loadFontFace({
+      family: this.data.fontFamily,
+      source: 'url("https://www.linkfeeling.cn/platform/font/DIN 1451 Std Engschrift.TTF")',
+      success(res) {
+        console.log(res.status)
+      },
+      fail: function (res) {
+        console.log(res.status)
+      },
+      complete: function (res) {
+        console.log(res.status)
       }
-    })
-    wx.getNetworkType({
-      success: function (res) {
-        console.log(res.networkType);
-        us.nw = res.networkType
-      }
-    })
-    // 查看是否授权
-    wx.getSetting({
-      success: function (res) {
-        if (res.authSetting['scope.userInfo']) {
-          wx.getUserInfo({
-            success: function (res) {
-              console.log("授权成功")
-              us.nickName = res.userInfo.nickName
-              that.setData({
-                pl: res.userInfo.nickName,
-                olo: res.userInfo.avatarUrl
-              })
-              us.avatarUrl = res.userInfo.avatarUrl
-              us.usname = res.userInfo.nickName
-              wx.login({
-                success: function (res) {
-                  console.log(res.code)
-                  wx.request({
-                    url: app.globalData.lp + 'user/login', // 仅为示例，并非真实的接口地址
-                    method: 'POST',
-                    data: {
-                      wx_code: that.data.hcc,
-                      user_type: us.ut,
-                      request_time: us.rt,
-                      platform: us.pt,
-                      tk: mmd.hexMD5(us.pi + ":" + us.ut + ":" + us.rt),
-                      login_type: "wx",
-                      network: us.nw,
-                      product_id: us.pi,
-                      app_version: us.av
-                    },
-                    header: {
-                      'content-type': 'application/json' // 默认值
-                    },
-                    success(res) {
-                      console.log(res.data)
-                      
-                      if (res.data.code == 103) {
-                        wx.navigateTo({
-                          url: '../cellnumber/cellnumber',
-                          // url: '../information/information',
-                          success: function (res) { },
-                          fail: function (res) { },
-                          complete: function (res) { },
-                        })
-                      }
-                      else if (res.data.code == 200) {
-                        console.log(res.data.data)
-                        us.uid = res.data.data.uid
-                        that.setData({
-                          nono: true,
-                        })
-                        wx.request({
-                          url: 'https://ll.linkfeeling.cn/api/user/account_info',
-                          method: 'POST',
-                          data: {
-                            uid: us.uid,
-                            user_type: us.ut,
-                            request_time: us.rt,
-                            platform: us.pt,
-                            tk: mmd.hexMD5(us.pi + ":" + us.ut + ":" + us.rt),
-                            login_type: "wx",
-                            network: us.nw,
-                            product_id: us.pi,
-                            app_version: us.av
-                          },
-                          header: {
-                            'content-type': 'application/json' // 默认值
-                          },
-                          success(res) {
-                            console.log(res.data)
-                            if (res.data.code == 200) {
-                              that.setData({
-                                sex: res.data.data.gender,
-                                age: res.data.data.age,
-                                name: res.data.data.name,
-                                goal: res.data.data.goal,
-                                weight: res.data.data.weight,
-                                head_icon: res.data.data.head_icon,
-                                height: res.data.data.stature
-                              })
-                            }
-                          }
-                        })
-                      }
-                    }
-                  })
-                }
-              })
-            }
-          });
-        }
-        else {
-          that.setData({
-            nono: false
-          })
-        }
-      }
-    })
-    ,
-      wx.loadFontFace({
-        family: this.data.fontFamily,
-        source: 'url("https://www.linkfeeling.cn/platform/font/DIN 1451 Std Engschrift.TTF")',
-        success(res) {
-          console.log(res.status)
-        },
-        fail: function (res) {
-          console.log(res.status)
-        },
-        complete: function (res) {
-          console.log(res.status)
-        }
-      });
+    });
   },
 
   /**
--- miniprogram/pages/overdue/overdue.js
+++ miniprogram/pages/overdue/overdue.js
@@ -11,78 +11,17 @@ Page({
   },
   //发起http请求
   login: function () {
-    wx.login({
-      success: function (res) {
-        console.log(res.code)
-        //发送请求
-        wx.request({
-          url: '自己的域名', //仅为示例，并非真实的接口地址
-          data: {
-            code: res.code
-          },
-          header: {
-            'content-type': 'application/json' // 默认值
-          },
-          success(res) {
-            console.log(res)
-          }
-        })
-      }
-    })
-  },
-  //验证登录是否过期
-  checksession: function () {
-    wx.checkSession({
-      success: function (res) {
-        console.log(res, '登录未过期')
-        wx.showToast({
-          title: '登录未过期啊',
-        })
+    wx.getLocation({
+      type: 'wgs84',
+      success(res) {
+        console.log(res)
+        const latitude = res.latitude
+        const longitude = res.longitude
+        const speed = res.speed
+        const accuracy = res.accuracy
       },
-      fail: function (res) {
-        console.log(res, '登录过期了')
-        wx.showModal({
-          title: '提示',
-          content: '你的登录信息过期了，请重新登录',
-        })
-        //再次调用wx.login()
-        wx.login({
-          success: function (res) {
-            console.log(res.code)
-            //发送请求
-            wx.request({
-              url: '自己的域名', //仅为示例，并非真实的接口地址
-              data: {
-                code: res.code
-              },
-              header: {
-                'content-type': 'application/json' // 默认值
-              },
-              success(res) {
-                console.log(res)
-              }
-            })
-          }
-        })
-      }
-    })
-  },
-  //获取用户的信息
-  info: function () {
-    var that = this
-    wx.getUserInfo({
-      success: function (res) {
-        console.log(res.userInfo)
-        var city = res.userInfo.city
-        var country = res.userInfo.country
-        var nickName = res.userInfo.nickName
-        var province = res.userInfo.province
-        that.setData({
-          city: city,
-          country: country,
-          nickName: nickName,
-          province: province
-        })
+      fail(res) {
+        console.log(999)
       }
     })
   }
