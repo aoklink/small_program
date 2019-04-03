717cc4c29b2034c665b945b44b1d4bc3f9d18ef2
yunhan <2670893251@qq.com>
2019-04-03 20:44:11 +0800

    运动报告首页开始

--- miniprogram/pages/lkfl/lkfl.js
+++ miniprogram/pages/lkfl/lkfl.js
@@ -1,11 +1,25 @@
-// pages/lkfl/lkfl.js
+var app = getApp()
+var us = getApp().globalData.userInfo
+var mmd = require('../../utils/mmd.js');
+const doAuth = require('../../utils/doAuth.js')
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    head: 'https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83er3rU0P2cN6o1hOkrrR8FDZ0zrbLiawQZsJ2ia6m3mAETuS1uJl3XhqO9dR0M7uPJc03ejichibQcsJAQ/132',
+    hname: '皮卡丘大战熏悟空',
+    haddress: '领客菲力健身房西湖银泰店',
+    hscore: '91',
+    otup: [
+      'https://ll.linkfeeling.cn/wx/a.png',
+      'https://ll.linkfeeling.cn/wx/f.png',
+      'https://ll.linkfeeling.cn/wx/d.png',
+      'https://ll.linkfeeling.cn/wx/b.png',
+      'https://ll.linkfeeling.cn/wx/e.png',
+      'https://ll.linkfeeling.cn/wx/c.png'
+    ]
   },
 
   /**
--- miniprogram/pages/lulu/lulu.js
+++ miniprogram/pages/lulu/lulu.js
@@ -1,4 +1,3 @@
-// pages/lulu/lulu.js
 Page({
 
   /**
